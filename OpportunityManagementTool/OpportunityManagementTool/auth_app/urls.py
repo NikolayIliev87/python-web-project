8fd4a8abf9811a91f45a106c5f492fd7d90569a4
NikolayIliev87 <103068553+NikolayIliev87@users.noreply.github.com>
2022-04-11 17:42:13 +0300

    landing site page and logout
    Landing site page buttons adjustments.
    Logout functionality.
    CSS&HTML small adjustments.

--- OpportunityManagementTool/OpportunityManagementTool/auth_app/urls.py
+++ OpportunityManagementTool/OpportunityManagementTool/auth_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
 from OpportunityManagementTool.auth_app.views import UserLoginView, UserCreateView, \
-    ChangeUserPasswordView, EditProfileView, UserDetailsView
+    ChangeUserPasswordView, EditProfileView, UserDetailsView, log_out_user
 
 urlpatterns = (
     path('login/', UserLoginView.as_view(), name='login user'),
     path('register/', UserCreateView.as_view(), name='request user'),
+    path('logout/', log_out_user, name='logout user'),
     path('profile/<int:pk>/', UserDetailsView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', EditProfileView.as_view(), name='edit profile'),
     path('edit-password/', ChangeUserPasswordView.as_view(), name='change password'),
